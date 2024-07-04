c5a37975cbabac77d3ef098a8f2867ea4f4d2f84
Carter Hesterman <hestermancarter@gmail.com>
2024-07-03 23:21:26 -0600

    Re add start room test

--- test/TestStartingRoom.py
+++ test/TestStartingRoom.py
@@ -8,40 +8,40 @@ from . import MetroidPrimeTestBase
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), 'test_output')
 
 
-# class TestStartingRoomsGenerate(MetroidPrimeTestBase):
-#     auto_construct = False
+class TestStartingRoomsGenerate(MetroidPrimeTestBase):
+    auto_construct = False
 
-#     def test_starting_room_rando_no_missile_launcher(self):
-#         for room_name in all_start_rooms:
-#             with self.subTest(f"Starting Room: {room_name}", room_name=room_name):
-#                 self.options = {
-#                     "starting_room_name": room_name,
-#                     "elevator_randomization": False,
-#                     "missile_launcher": 0,
-#                 }
-#                 try:
-#                     self.world_setup()
-#                     distribute_items_restrictive(self.multiworld)
-#                     self.assertBeatable(True)
-#                 except Exception as e:
-#                     self.fail(f"Failed to generate beatable game with start room: {room_name}. ")
+    def test_starting_room_rando_no_missile_launcher(self):
+        for room_name in all_start_rooms:
+            with self.subTest(f"Starting Room: {room_name}", room_name=room_name):
+                self.options = {
+                    "starting_room_name": room_name,
+                    "elevator_randomization": False,
+                    "missile_launcher": 0,
+                }
+                try:
+                    self.world_setup()
+                    distribute_items_restrictive(self.multiworld)
+                    self.assertBeatable(True)
+                except Exception as e:
+                    self.fail(f"Failed to generate beatable game with start room: {room_name}. ")
 
-#     def test_starting_room_rando_with_missile_launcher(self):
-#         for room_name in all_start_rooms:
-#             with self.subTest(f"Starting Room: {room_name}", room_name=room_name):
-#                 self.options = {
-#                     "starting_room_name": room_name,
-#                     "elevator_randomization": False,
-#                     "missile_launcher": 1,
-#                 }
-#                 try:
-#                     self.world_setup()
-#                     distribute_items_restrictive(self.multiworld)
-#                     self.assertBeatable(True)
-#                     self.multiworld = None
-#                     self.world = None
-#                 except Exception as e:
-#                     self.fail(f"Failed to generate beatable game with start room: {room_name}. ")
+    def test_starting_room_rando_with_missile_launcher(self):
+        for room_name in all_start_rooms:
+            with self.subTest(f"Starting Room: {room_name}", room_name=room_name):
+                self.options = {
+                    "starting_room_name": room_name,
+                    "elevator_randomization": False,
+                    "missile_launcher": 1,
+                }
+                try:
+                    self.world_setup()
+                    distribute_items_restrictive(self.multiworld)
+                    self.assertBeatable(True)
+                    self.multiworld = None
+                    self.world = None
+                except Exception as e:
+                    self.fail(f"Failed to generate beatable game with start room: {room_name}. ")
 
 
 class TestStartingRoomsGenerateWithElevatorRando(MetroidPrimeTestBase):
