b1c648edcbf47a63387477df822ceca165962452
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2024-08-09 13:26:44 -0500

    Fixed: missing imports in TallonOverworld.py

--- data/TallonOverworld.py
+++ data/TallonOverworld.py
@@ -1,5 +1,5 @@
 from BaseClasses import CollectionState
-from ..Logic import can_bomb, can_boost, can_charge_beam, can_grapple, can_missile, can_morph_ball, can_move_underwater, can_power_beam, can_power_bomb, can_space_jump, can_spider, can_super_missile, can_xray
+from ..Logic import can_bomb, can_boost, can_charge_beam, can_grapple, can_missile, can_morph_ball, can_move_underwater, can_power_beam, can_power_bomb, can_space_jump, can_spider, can_super_missile, can_xray, can_wave_beam, can_thermal
 from ..LogicCombat import can_combat_ghosts
 from .Tricks import Tricks
 from .AreaNames import MetroidPrimeArea
